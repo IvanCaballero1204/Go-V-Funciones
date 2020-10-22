9aed6167b220099c08089d0b839c5f54fc437604
repl.it user <replituser@example.com>
2020-10-22 02:39:09 +0000

    Funciones

--- funciones.go
+++ funciones.go
@@ -12,14 +12,13 @@ func Fibonacci(n int) int {
 }
 
 func MasGrande(args ...int) int {
-	temp := args[0]
-
-	for _, v := range args {
+  temp := args[0]
+  
+  for _, v := range args {
     if(temp < v){
       temp = v
-    }
-	}
-
+      }
+  }
 	return temp
 }
 
@@ -34,28 +33,23 @@ func generadorImpares() func() int {
 
 func main(){
   var temp, p int
-	var slice []int
- 	nextImpar := generadorImpares()
-
-
+  var slice []int
+  nextImpar := generadorImpares()
   fmt.Print("Ingrese posición para fiboniacci: ")
   fmt.Scan(&temp)
   fmt.Println("Numero Fibonacci", Fibonacci(temp))
-
   fmt.Print("Ingrese longitud del arreglo: ")
   fmt.Scan(&p)
-
   for i:=0; i < p; i++{
     fmt.Print("Ingrese numero: ")
     fmt.Scan(&temp)
     slice = append(slice, temp)
   }
-
+  
   fmt.Println("El número más grande fue", MasGrande(slice...))
-
   fmt.Print("Ingrese cantidad de numeros impares: ")
   fmt.Scan(&temp)
-
+  
   for i:=1; i <= temp; i++{
     fmt.Println(nextImpar())
   }
